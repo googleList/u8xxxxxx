f3cbed1c6d8932bab6475ef7fb654bed3ed19624
gtjmslg <152575509+googleList@users.noreply.github.com>
2025-08-01 10:36:39 +0800

    Add files via upload

---
+++ assesr/script.js
@@ -0,0 +1,10 @@
+var images = ['assesr/text1.png', 'assesr/text2.png'];
+// var images = ['assesr/text1.png'];
+var currentIndex = 0;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("slider").src = "assesr/text1.png";
+    $('.main-title img').css('width', '90%');
+    $('.main-title').css('top', '13%');
+});
+ 
